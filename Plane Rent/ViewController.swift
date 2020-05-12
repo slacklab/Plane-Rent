ba33e1d06c32e0b77314d4cf4c9c70d5194ad70d
Ivan Sorokoletov <i.sorokoletov@gettransfer.com>
2020-05-12 04:20:00 +0300

    change to use storyboards!

---
+++ Plane Rent/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Plane rent
+//
+//  Created by Ivan Sorokoletov on 12.05.2020.
+//  Copyright © 2020 Slacklab. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
