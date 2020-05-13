66672856d9dfe4ac6e998649e81b1c16fd364789
Ivan Sorokoletov <i.sorokoletov@gettransfer.com>
2020-05-13 11:09:48 +0300

    some further

---
+++ Plane Rent/Screens/RentPlaneVC.swift
@@ -0,0 +1,30 @@
+//
+//  RentPlaneVC.swift
+//  Plane rent
+//
+//  Created by Ivan Sorokoletov on 13.05.2020.
+//  Copyright © 2020 Slacklab. All rights reserved.
+//
+
+import UIKit
+
+class RentPlaneVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
