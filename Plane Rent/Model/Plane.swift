ead49c142371b6ca5ea27c7a2ab3d9d72a015a1d
Ivan Sorokoletov <i.sorokoletov@gettransfer.com>
2020-05-18 09:36:29 +0300

    make list for app for diff api

--- Plane Rent/Model/Plane.swift
+++ Plane Rent/Model/Plane.swift
@@ -14,12 +14,20 @@ import Foundation
 //21.09.2029","ts_min_flight":"90","ts_min_this_type_flight":"20","ts_check_flight":"Нет","ts_flight_to_airfields":"10","plane_image":"13_05_202001_41_275993358image
 //(1).jpg"}]}
 
+
+//{"air_airplanes":[{"plane_id":"2","plane_model":"Boeing-747","plane_base":"Домодедово","plane_price":"55000","plane_ownerid":"66","ts_right":"Аренда","ts_certificate":"Типовой","ts_CWS":"Нет","ts_min_flight":"3","ts_min_this_type_flight":"1","ts_check_flight":"Нет","ts_flight_to_airfields":"Нет","plane_image":"boeing_747_24.jpg","user_phone":"79067092065"},{"plane_id":"6","plane_model":"N800
+//2","plane_base":"Шереметьево","plane_price":"600","plane_ownerid":"66","ts_right":"Собственность","ts_certificate":"Типовой","ts_CWS":"20.06.2021","ts_min_flight":"70","ts_min_this_type_flight":"10","ts_check_flight":"Нет","ts_flight_to_airfields":"5","plane_image":"14_05_202001_55_072525876image
+//(1).jpg","user_phone":"79067092065"},{"plane_id":"12","plane_model":"N800JP","plane_base":"Шереметьево","plane_price":"5000","plane_ownerid":"78","ts_right":"Собственность","ts_certificate":"ЕЭВС","ts_CWS":"До
+//20.03.2021","ts_min_flight":"90","ts_min_this_type_flight":"20","ts_check_flight":"Нет","ts_flight_to_airfields":"5","plane_image":"15_05_202003_58_101178742image
+//(1).jpg","user_phone":"79197762070"}]}
+
 /// This need for main `air_airplanes` dictionary
 class Planes: Codable {
     let air_airplanes: [Plane]
     
-    init(air_planes: [Plane]) {
-        self.air_airplanes = air_planes
+    init(air_airplanes: [Plane]) {
+//        self.air_airplanes = air_planes
+    self.air_airplanes = air_airplanes
     }
 }
 
@@ -37,6 +45,7 @@ class Plane: Codable {
     let ts_check_flight: String
     let ts_flight_to_airfields: String
     let plane_image: String
+    let user_phone: String
     
     init(plane_id: String,
          plane_model: String,
@@ -50,7 +59,8 @@ class Plane: Codable {
          ts_min_this_type_flight: String,
          ts_check_flight: String,
          ts_flight_to_airfields: String,
-         plane_image: String) {
+         plane_image: String,
+         user_phone: String) {
         
         self.plane_id = plane_id
         self.plane_model = plane_model
@@ -65,5 +75,6 @@ class Plane: Codable {
         self.ts_check_flight = ts_check_flight
         self.ts_flight_to_airfields = ts_flight_to_airfields
         self.plane_image = plane_image
+        self.user_phone = user_phone
     }
 }
--- Plane Rent/Screens/RentPlaneVC.swift
+++ Plane Rent/Screens/RentPlaneVC.swift
@@ -7,3 +7,120 @@
 //
 
 import Foundation
+import UIKit
+
+class RentPlaneVC: BaseViewController {
+    let defaults = UserDefaults.standard
+    
+    @IBOutlet weak var mainImage: UIImageView!
+    
+    @IBOutlet weak var modelLabel: UILabel!
+    @IBOutlet weak var baseLabel: UILabel!
+    @IBOutlet weak var priceLabel: UILabel!
+    
+    @IBOutlet weak var rentDatePicker: UIDatePicker!
+    
+    let localeId = Locale.preferredLanguages.first
+    var dateRent = ""
+    
+    var selectedCell: Int = 0
+    var planes = [Plane]()
+    let planeImagesDir = "http://big-marka.xyz/plane_images/"
+    
+    @IBAction func doneButton(_ sender: Any) {
+        let isDateRentRight = !dateRent.isEmpty
+        
+        if isDateRentRight {
+            let bookedVC = self.storyboard?.instantiateViewController(
+                withIdentifier: "BookedVC"
+                ) as! BookedVC
+            
+            self.navigationController!.pushViewController(bookedVC, animated: true)
+            // TODO: add ban for send sms
+            sendNeedMessage()
+            
+        } else {
+            print("rent date wrong")
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        rentDatePicker.locale = Locale(identifier: localeId ?? "")
+        rentDatePicker.addTarget(self, action: #selector(datePickerChanged), for: .valueChanged)
+        
+        modelLabel.text = planes[selectedCell].plane_model
+        baseLabel.text = planes[selectedCell].plane_base
+        priceLabel.text = planes[selectedCell].plane_price
+        
+        downloadImageAndSet()
+        
+    }
+    
+    override func dismissKeyboardOnTap() {
+        
+    }
+    
+    @objc func datePickerChanged(_: Any) {
+        getDateFromPicker()
+    }
+    
+    func getDateFromPicker() {
+        let formatter = DateFormatter()
+        
+        formatter.dateFormat = "dd.MM.yyyy HH:MM"
+        
+        dateRent = formatter.string(from: rentDatePicker.date)
+        
+        #if DEBUG
+        print(dateRent)
+        #endif
+    }
+    
+    func downloadImageAndSet() {
+        if let imageURL = URL(string:
+            ((planeImagesDir + planes[selectedCell].plane_image)
+                .addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) ?? ""
+        )) {
+            DispatchQueue.global().async {
+                let data = try? Data(contentsOf: imageURL)
+                if let data = data {
+                    let image = UIImage(data: data)
+                    DispatchQueue.main.async {
+                        self.mainImage.image = image
+                    }
+                }
+            }
+        }
+    }
+    
+    func sendNeedMessage() {
+        
+        // MARK: - Get values from UserDefaults
+        
+        guard let valueCurrentAccountType = defaults.string(forKey:
+            UserDefaultList.currentAccountType) else { return }
+        guard let valuePhoneNumberOfCurrentUser = defaults.string(forKey: UserDefaultList.currentPhoneNumberOfUser) else { return }
+        
+        guard let valueCurrentUserName = defaults.string(forKey: UserDefaultList.currentAccountName) else { return }
+        
+        guard let valueCurrentUserLastName = defaults.string(forKey: UserDefaultList.currentAccountLastName) else { return }
+        
+        #if DEBUG
+        print(valueCurrentAccountType)
+        print(valuePhoneNumberOfCurrentUser)
+        print(valueCurrentUserName)
+        print(valueCurrentUserLastName)
+        #endif
+        
+        
+        switch valueCurrentAccountType {
+        case AccountType.passenger:
+            SmsService.send(phone: planes[selectedCell].user_phone,
+                            message: Constant.appName + "\(valueCurrentUserName) \(valueCurrentUserLastName) хочет арендовать у вас \(planes[selectedCell].plane_model) \(dateRent) в качестве пассажира. Телефон: \(valuePhoneNumberOfCurrentUser)")
+        default:
+            print("not set for send sms")
+        }
+    }
+}
