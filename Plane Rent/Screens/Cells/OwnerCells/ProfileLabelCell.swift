6de5014046ffabaae8c447d01d76905a49a18103
Ivan Sorokoletov <i.sorokoletov@gettransfer.com>
2020-05-19 10:03:31 +0300

    wip add cells for owner vc, need connect ids

---
+++ Plane Rent/Screens/Cells/OwnerCells/ProfileLabelCell.swift
@@ -0,0 +1,24 @@
+//
+//  ProfileLabelCell.swift
+//  Plane rent
+//
+//  Created by Ivan Sorokoletov on 19.05.2020.
+//  Copyright © 2020 Slacklab. All rights reserved.
+//
+
+import UIKit
+
+class ProfileLabelCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
