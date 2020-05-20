ffc2f59992314593e42b514954bcc35ace0fecc6
Ivan Sorokoletov <i.sorokoletov@gettransfer.com>
2020-05-20 05:34:02 +0300

    fix for open pilot, owner vcs

---
+++ Plane Rent/Screens/AircraftLists/PlaneCell.swift
@@ -0,0 +1,30 @@
+//
+//  PlaneCell.swift
+//  Plane rent
+//
+//  Created by Ivan Sorokoletov on 13.05.2020.
+//  Copyright © 2020 Slacklab. All rights reserved.
+//
+
+import UIKit
+
+/// TODO: - now this cell is universall for plane and helicopter
+class PlaneCell: UITableViewCell {
+
+    @IBOutlet weak var planeModelLabel: UILabel!
+    @IBOutlet weak var planeAirportLabel: UILabel!
+    @IBOutlet weak var planePriceLabel: UILabel!
+    @IBOutlet weak var planeImage: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
