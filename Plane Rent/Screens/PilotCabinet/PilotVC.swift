52142e8ecd0bf4b88a7d19e95480661a43c1c61e
Ivan Sorokoletov <i.sorokoletov@gettransfer.com>
2020-05-20 05:51:17 +0300

    Set VC for pilot, owner screens

---
+++ Plane Rent/Screens/PilotCabinet/PilotVC.swift
@@ -0,0 +1,30 @@
+//
+//  PilotVC.swift
+//  Plane rent
+//
+//  Created by Ivan Sorokoletov on 20.05.2020.
+//  Copyright © 2020 Slacklab. All rights reserved.
+//
+
+import UIKit
+
+class PilotVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
