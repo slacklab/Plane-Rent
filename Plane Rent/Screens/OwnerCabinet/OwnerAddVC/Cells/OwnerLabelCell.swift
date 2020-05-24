1beb9b66d507899e617a8333a75e46d3efe7e8e7
Ivan Sorokoletov <i.sorokoletov@gettransfer.com>
2020-05-24 10:33:59 +0300

    form cells for add aircraft

---
+++ Plane Rent/Screens/OwnerCabinet/OwnerAddVC/Cells/OwnerLabelCell.swift
@@ -0,0 +1,26 @@
+//
+//  OwnerLabelCell.swift
+//  Plane rent
+//
+//  Created by Ivan Sorokoletov on 24.05.2020.
+//  Copyright © 2020 Slacklab. All rights reserved.
+//
+
+import UIKit
+
+class OwnerLabelCell: UITableViewCell {
+
+    @IBOutlet weak var mainLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
--- Plane Rent/Screens/OwnerCabinet/OwnerAddVC/OwnerAddVC.swift
+++ Plane Rent/Screens/OwnerCabinet/OwnerAddVC/OwnerAddVC.swift
@@ -9,23 +9,50 @@
 import UIKit
 
 class OwnerAddVC: BaseViewController, UITableViewDelegate, UITableViewDataSource {
-
+    
     @IBOutlet weak var tableView: UITableView!
+    
+    // Cell indexes
+    let cellIndexAddImage = 0
+    let cellIndexCheckHelicopter = 1
+    let cellIndexCheckPlane = 2
+    let cellIndexModelTextField = 3
+    let cellIndexPriceTextField = 4
+    let cellIndexAircraftOwnType = 5
+    let cellIndexBaseTextField = 6
+    let cellIndexEAWSOrTypeTextfield = 7
+    let cellIndexSLGTextField = 8
+    let cellIndexMinReqsLabel = 9
+    let cellIndexFlightBaseHours = 10
+    let cellIndexFlightTypeHours = 11
+    let cellIndexCheckFlightLabel = 12
+    let cellIndexCheckFlightYes = 13
+    let cellIndexCheckFlightNo = 14
+    let cellIndexCheckFlightHours = 15
+    let cellIndexAerodromOpportunityLabel = 16
+    let cellIndexCheckAerodromOpportunityYes = 17
+    let cellIndexCheckAerodromOpportunityNo = 18
+    let cellIndexMinHoursForPodlet = 19
+    let cellIndexDoneButton = 20
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.title = "Добавить ЛА"
+        
+        tableView.dataSource = self
+        tableView.delegate = self
 
+        
         // Register all xib cells
         
         let nibNameRight = UINib(nibName: "AddImageCell", bundle: nil)
         tableView.register(nibNameRight, forCellReuseIdentifier: "AddImageCell")
         
-        let nibName = UINib(nibName: "TypeTitleCell", bundle: nil)
-        tableView.register(nibName, forCellReuseIdentifier: "TypeTitleCell")
+        let nibName = UINib(nibName: "TextFieldCell", bundle: nil)
+        tableView.register(nibName, forCellReuseIdentifier: "TextFieldCell")
         
-        let nibNameAir = UINib(nibName: "AircraftCell", bundle: nil)
-        tableView.register(nibNameAir, forCellReuseIdentifier: "AircraftCell")
+        let nibNameAir = UINib(nibName: "CheckCell", bundle: nil)
+        tableView.register(nibNameAir, forCellReuseIdentifier: "CheckCell")
         
         tableView.tableFooterView = UIView()
     }
@@ -39,6 +66,16 @@ class OwnerAddVC: BaseViewController, UITableViewDelegate, UITableViewDataSource
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        
+        if indexPath.row == 0 {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "AddImageCell", for: indexPath) as! AddImageCell
+            cell.commonInit(image: R.image.bgLaunchPlane()!)
+            
+            return cell
+        }
+        
+        
         return UITableViewCell()
     }
 }
